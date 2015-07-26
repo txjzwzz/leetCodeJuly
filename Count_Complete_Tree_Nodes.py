4fbf8b9955572e445cc72ec51b79cc2b12de306d
txjzwzz <txjzwzz@gmail.com>
2015-07-26 08:39:12 +0800

    start count complete tree nodes

--- Count_Complete_Tree_Nodes.py
+++ Count_Complete_Tree_Nodes.py
@@ -16,5 +16,59 @@ class TreeNode:
 class Solution:
     # @param {TreeNode} root
     # @return {integer}
+    # 由于还是需要遍历一下底层的点，所以广度优先可行，就是可能需要大量的存储空间
+    # 超时
+    """
     def countNodes(self, root):
-                            
+        if not root:
+            return 0
+        queue = [root]
+        depth = 0
+        while True:
+            tmpQueue = []
+            startIndex = 0
+            while startIndex < len(queue):
+                tmpNode = queue[startIndex]
+                startIndex += 1
+                if tmpNode.left:
+                    tmpQueue.append(tmpNode.left)
+                else:
+                    return pow(2, depth+1) - 1 + len(tmpQueue)
+                if tmpNode.right:
+                    tmpQueue.append(tmpNode.right)
+                else:
+                    return pow(2, depth+1) - 1 + len(tmpQueue)
+            queue = tmpQueue[:]
+            depth += 1
+    """
+    def countNodes(self, root):
+        return
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    root = None
+    print solution.countNodes(root)
+    root = TreeNode(10)
+    print solution.countNodes(root)
+    l1 = TreeNode(11)
+    root.left = l1
+    print solution.countNodes(root)
+    r1 = TreeNode(12)
+    root.right = r1
+    print solution.countNodes(root)
+    ll1 = TreeNode(13)
+    l1.left = ll1
+    print solution.countNodes(root)
+    lr1 = TreeNode(14)
+    l1.right = lr1
+    print solution.countNodes(root)
+    rl1 = TreeNode(15)
+    r1.left = rl1
+    print solution.countNodes(root)
+    rr1 = TreeNode(16)
+    r1.right = rr1
+    print solution.countNodes(root)
+    lll1 = TreeNode(17)
+    ll1.left = lll1
+    print solution.countNodes(root)
